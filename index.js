2861c638f4ed345dd94e9dcc8b82888f720af70e
thingMaker <83979271+thingMake@users.noreply.github.com>
2021-11-25 19:09:23 +0000

    Add db.js, it won't be used yet.

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(cors({
 }))
 const Database = require("@replit/database");
 const db = new Database()
+//const db2 = require("./db.js")
 const bcrypt = require('bcrypt')
 const WebSocketServer = require('websocket').server;
 const url = require('url');
