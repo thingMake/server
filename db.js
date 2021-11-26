2cf999c40b9868b0a9b84c5589df4a2e4ff0c088
thingMaker <83979271+thingMake@users.noreply.github.com>
2021-11-26 19:18:55 +0000

    Add list function to db.js

--- db.js
+++ db.js
@@ -8,15 +8,15 @@ if (!db.supports.permanence) {
   throw new Error('Persistent storage is required')
 }
 
-  db.get('name', function (err, value) {
-    if (err) return console.log('Ooops!', err) // likely the key was not found
+/*db.get('name', function (err, value) {
+  if (err) return console.log('Ooops!', err) // likely the key was not found
 
-    console.log('name=' + value)
-  })
+  console.log('name=' + value)
+})*/
 
 module.exports = {
   get:async function(key){
-    var value = await db.get(key)
+    var value = await db.get(key).catch(() => null)
     if(value){
       return JSON.parse(value)
     }else{
@@ -30,5 +30,31 @@ module.exports = {
   delete:async function(key){
     await db.del(key)
     return this
+  },
+  list:function(prefix, values){
+    return new Promise(function(resolve, reject){
+      var obj = values ? {} : []
+      db.createReadStream()
+      .on('data', function (data) {
+        if(prefix && !data.key.startsWith(prefix)){
+          return
+        }
+        if(values){
+          obj[data.key] = JSON.parse(data.value)
+        }else{
+          obj.push(data.key)
+        }
+      })
+      .on('error', function (err) {
+        console.log('Oh my!', err)
+        reject(err)
+      })
+      .on('close', function () {
+        //console.log('Stream closed')
+      })
+      .on('end', function () {
+        resolve(obj)
+      })
+    })
   }
 }
