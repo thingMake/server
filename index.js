683630f89e84928215ecadcc645c72375a6c910c
thingMaker <83979271+thingMake@users.noreply.github.com>
2022-05-26 01:13:45 +0000

    Change panorama

--- index.js
+++ index.js
@@ -226,7 +226,7 @@ router.get('/log', async(req,res) => {
   res.sendFile(__dirname+"/pfp.png")
 })*/
 router.get("/panorama", (req,res) => {
-  res.redirect("https://data.thingmaker.repl.co/images/panorama/redstone.png")
+  res.redirect("https://data.thingmaker.repl.co/images/panorama/summer.png")
 })
 
 router.get("/common.js", (req,res) => {
